f45016e2a9786b6b74fbb4f173fb66fcb782c11c
Leszek Ucinski <rahvinos@gmail.com>
2022-06-07 18:58:44 +0200

    Add Navbar and Cognito Auth

--- course-blog/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ course-blog/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "courseblog": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "courseblog8649d751": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
